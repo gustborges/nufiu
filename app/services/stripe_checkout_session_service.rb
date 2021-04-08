6d9ab752c12d7f254647b9eb6eefde829ba14c87
Gustavo Borges <gustborges@gmail.com>
2021-04-08 13:19:33 -0300

    tried to fix pundit problems

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -1,9 +1,8 @@
 class StripeCheckoutSessionService
-  skip_after_action :verify_authorized
-  skip_before_action :authenticate_user!
 
   def call(event)
     cart = policy_scope(Cart).find_by(checkout_session_id: event.data.object.id)
+    authorize cart
     cart.update(state: 'paid')
   end
 end
