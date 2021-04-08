d129860f3d2ec21be8025dded10182bb20d7ee69
Gustavo Borges <gustborges@gmail.com>
2021-04-08 13:54:13 -0300

    Another one

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -1,7 +1,7 @@
 class StripeCheckoutSessionService
 
   def call(event)
-    cart = Cart.where(user: current_user).find_by(checkout_session_id: event.data.object.id)
+    cart = Cart.find_by(checkout_session_id: event.data.object.id)
     cart.update(state: 'paid')
   end
 end
