d5af610b76bfdc03d59f0f93edb7f54be1a63448
Gustavo Borges <gustborges@gmail.com>
2021-04-08 13:33:00 -0300

    minor fix

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -1,7 +1,7 @@
 class StripeCheckoutSessionService
 
   def call(event)
-    cart = policy_scope(Cart).find_by(checkout_session_id: event.data.object.id)
+    cart = policy_scope(Cart).where(user: current_user).find_by(checkout_session_id: event.data.object.id)
     authorize cart
     cart.update(state: 'paid')
   end
