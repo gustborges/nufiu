6b7d54870056c1d0b6fd0ba6860ff67b13d97c83
Gustavo Borges <gustborges@gmail.com>
2021-04-15 01:47:22 -0300

    Fixed navbar

--- app/javascript/packs/navbar_slider.js
+++ app/javascript/packs/navbar_slider.js
@@ -1,4 +1,5 @@
 const navSlide = () => {
+  const corpo = document.getElementsByTagName("BODY")[0];
   const burger = document.querySelector('.burger');
   const navLinks = document.querySelector('.navlinks');
   const navItems = document.querySelectorAll('.navlinks li');
@@ -6,24 +7,25 @@ const navSlide = () => {
   burger.addEventListener('click', ()=>{
     navLinks.classList.toggle('navlinks');
     navLinks.classList.toggle('nav-active');
-    if (navLinks.classList.contains('nav-active')) {
-      document.body.style['overflow-y'] = 'hidden';
-    } else {
-      document.body.style['overflow-y'] = 
-      'visible';
-    }
+    // if (navLinks.classList.contains('nav-active')) {
+    //   document.corpo.style['overflow-y'] = 'hidden';
+    //   console.log('asdasd');
+    // } else {
+    //   document.corpo.style['overflow-y'] = 
+    //   'visible';
+    // }
     
       // Animate Links
       navItems.forEach((link, index) => {
         if(link.style.animation) {
           link.style.animation = '';
-          body.classList.add('.stop-y');
         } else {
           link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.6}s`;
         }
       });
       // Burger animation
       burger.classList.toggle('toggle');
+      corpo.classList.toggle('stop-scrolling');
   });
 }
 
@@ -32,7 +34,6 @@ const navScroll = () => {
   window.onscroll = function() {
     var currentScrollPos = window.pageYOffset;
     if (currentScrollPos > 80) {
-      console.log('currentScrollPos > 30')
       nav.style.transform = "translateY(-100%)";
     } else {
       nav.style.transform = "translateY(0%)";
