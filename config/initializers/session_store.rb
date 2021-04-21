fe92aa62ae5f97e8f19a0cd16323af385e384ab6
Gustavo Borges <gustborges@gmail.com>
2021-04-21 11:46:07 -0300

    sessions configuration

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,4 @@
-# SessionStore::Application.config.session_store :cookie_store, {
-#   key: 'cart',
-#   domain: '.nufiu.com.br',
-#   expire_after: 24.hours
-# }
+Rails.application.config.session_store :cookie_store, {
+  key: 'cart',
+  expire_after: 1.days
+}
