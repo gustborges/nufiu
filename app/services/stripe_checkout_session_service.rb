bef73ed619b754d527e8b8783cbd593f57bf4ff5
Gustavo Borges <gustborges@gmail.com>
2021-04-26 16:57:40 -0300

    fixed checkout

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -1,6 +1,7 @@
 class StripeCheckoutSessionService
   def call(event)
     cart = Cart.find_by(checkout_session_id: event.data.object.id)
-    cart.update(state: 'paid')
+    cart.state = 'paid'
+    cart.save
   end
 end
