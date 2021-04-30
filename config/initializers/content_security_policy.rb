66fecbfef689c2d788283c6c91e5ad3bb7982d9c
Gustavo Borges <gustborges@gmail.com>
2021-04-30 01:33:32 -0300

    Fixing analytics problem

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -9,7 +9,7 @@
 #   policy.font_src    :self, :https, :data
 #   policy.img_src     :self, :https, :data
 #   policy.object_src  :none
-#   policy.script_src  :self, :https
+#   policy.script_src  :self, 'https://www.googletagmanager.com', 'https://www.google-analytics.com'
 #   policy.style_src   :self, :https
 #   # If you are using webpack-dev-server then specify webpack-dev-server host
 #   policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
