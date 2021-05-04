98f817ed694efc7a88a3d0c87b162929d1944bdb
Gustavo Borges <gustborges@gmail.com>
2021-05-04 19:18:55 -0300

    Deleted some js and css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,4 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+// Automatic compiled by Webpacker
 
 require("@rails/ujs").start();
 require("turbolinks").start();
@@ -27,8 +24,7 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
-
-document.addEventListener("turbolinks:load", () => {
+// document.addEventListener("turbolinks:load", () => {
   // Call your functions here, e.g:
   // initSelect2();
-});
+// });
